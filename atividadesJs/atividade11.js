95bdcbdfa415353a8e0e847359801c81eff0ea72
Vinicius Leonel <viniciusleonel1444@gmail.com>
2025-10-04 11:43:03 -0300

    Update atividade11.js

--- atividadesJs/atividade11.js
+++ atividadesJs/atividade11.js
@@ -1,1 +1,21 @@
+const readline = require('readline-sync');
+
+let nome = readline.question("Insira o nome do cliente: ");
+let idade = Number(readline.question("Insira a idade do cliente: "));
+let valor  = Number(readline.question("Insira o valor do ingresso: R$ "));
+
+if (idade < 18) {
+    console.log("O cliente não pode comprar, pois é menor de idade.");
+} else {
+    if (valor >= 100) {
+        let desconto = valor * 0.20;
+        valor = valor - desconto;
+        console.log(`Desconto aplicado de R$${desconto.toFixed(2)}!`);
+    }
+
+    console.log(`Cliente: ${nome}, Idade: ${idade}, Valor final: R$${valor.toFixed(2)}`);
+}
+
+console.log('------------------------ ')
+  
 
